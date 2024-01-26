6f4a16ee1f61bfbc7002b171e9cbd816ed797fcd
Ladycaiza <150860665+Ladycaiza@users.noreply.github.com>
2024-01-25 20:44:03 -0500

    Add files via upload

---
+++ Ejemplos de Python/clasesyobjetos.py
@@ -0,0 +1,17 @@
+
+class Celular:
+    def __init__(self, marca, modelo, camara) :
+        self.marca = marca
+        self.modelo = modelo
+        self.camara = camara
+
+    # def funcionxd(xd):
+      #  print(xd)
+
+celular1 = Celular("Samsung","S23", "48MP")
+celular2 = Celular("Apple","Iphone 15 Pro", "96MP")
+celular3 = Celular("Nokia", "C3", "12MP")
+
+print(celular3.marca)
+    
+
---
+++ Ejemplos de Python/funciones.py
@@ -0,0 +1,22 @@
+
+class Celular:
+    def __init__(self, marca, modelo, camara) :
+        self.marca = marca
+        self.modelo = modelo
+        self.camara = camara
+
+    def llamar(self):
+        print(f"Estas haciendo una llamado desde un: {self.modelo}")
+
+    def cortar(self):
+        print(f"Cortaste la llamada desde tu: {self.modelo}")
+
+celular1 = Celular("Samsung","S23", "48MP")
+celular2 = Celular("Apple","Iphone 15 Pro", "96MP")
+celular3 = Celular("Nokia", "C3", "12MP")
+
+celular2.llamar()
+
+    
+
+ 
